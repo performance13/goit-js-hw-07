06761bfe3ae49cd82ec54f9b01010c18afa71315
Anatolii Kotenko <akotenko1313@gmail.com>
2021-11-07 00:24:52 +0200

    11.07.22

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import "./task-01.js"
+import "./task-02.js"
+import "./task-03.js"
+import "./task-04.js"
+import "./task-05.js"
+import "./task-06.js"
+import "./task-07.js"
+import "./task-08.js"
